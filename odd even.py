0c8647ccfb74f295799bb938576357b389d2ccb4
princemaxwell2011 <ajoseph379@gmail.com>
2024-12-28 15:43:37 +0100

    first commit

---
+++ odd even.py
@@ -0,0 +1,6 @@
+num =int(input("enter number to check whether it is odd or even"))
+print("the number to be checked is ",num)
+if num%2==0:
+    print("this is a prime number")
+else:
+     print("this is an odd number")
